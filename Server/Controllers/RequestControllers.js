1bf05978c785083026efc38d3b9a491f6679ad1f
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-10-08 16:52:38 +0100

    request routes , added controlleres for request adding

--- Server/Controllers/RequestControllers.js
+++ Server/Controllers/RequestControllers.js
@@ -1,5 +1,24 @@
+const { request } = require("express");
 const db = require("../Database/index");
 
 module.exports = {
-     
+    addRequest: async (req, res) => {
+        try {
+            const filter = {
+                email: req.body.email
+            }
+            
+            const requestidentif = await db.Patients.findOne({ where: filter })
+            
+            const requestForm = {
+                description: req.body.description,
+                status: req.body.status,
+                patientId : requestidentif.id
+            }
+            const request = await db.requests.create(requestForm)
+            res.status(201).json(requestidentif)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
