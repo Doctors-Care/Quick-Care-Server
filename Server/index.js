5de066c5e1dc12adbde1d6a49c2671851761cc17
Moez-Temimi <moez.temimi@yahoo.com>
2022-10-09 12:49:25 +0200

    working on deployement of DB

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,26 @@ const express = require("express");
 const cors = require ('cors')
 const app = express();
 var PORT = process.env.PORT || 3000
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 db.sequelize.sync().then(()=>console.log("t3adet")).catch((err)=>console.log(err))
@@ -22,11 +42,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-app.use("/doctor",doctorRoutes) ;
-app.use("/user",patientRoutes) ;
-app.use("/hce",hceRoutes) ;
-app.use("/ambulance",ambulanceRoute) ;
-app.use("/request",requestRoute)
+// app.use("/doctor",doctorRoutes) ;
+// app.use("/user",patientRoutes) ;
+// app.use("/hce",hceRoutes) ;
+// app.use("/ambulance",ambulanceRoute) ;
+// app.use("/request",requestRoute)
 
 
 
