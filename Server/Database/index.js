0acefa60e53baf4c77205c9607b33a2df8711292
Moez-Temimi <moez.temimi@yahoo.com>
2022-10-11 11:52:24 +0200

    dione

--- Server/Database/index.js
+++ Server/Database/index.js
@@ -1,10 +1,16 @@
 const { Sequelize, DataTypes } = require("sequelize");
+const config = require("../../config.json")
 
-
-const sequelize = new Sequelize(process.env.DATABASE_URL || "postgres://lxysmkhoieuclr:639588451789e1e6d736a2962f6ef41a363dacad34c6ef54c2c3ba8e27875437@ec2-54-75-184-144.eu-west-1.compute.amazonaws.com:5432/d9hln4u4bbmm6n",{ dialectOptions:{ssl: {
-    rejectUnauthorized: false
-  }}})
-
+// const sequelize = new Sequelize(process.env.DATABASE_URL || "postgres://lxysmkhoieuclr:639588451789e1e6d736a2962f6ef41a363dacad34c6ef54c2c3ba8e27875437@ec2-54-75-184-144.eu-west-1.compute.amazonaws.com:5432/d9hln4u4bbmm6n",{ dialectOptions:{ssl: {
+//     rejectUnauthorized: false
+//   }}})
+const sequelize = new Sequelize(config.database, config.user, config.password,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  }
+);
   const db = {};
   db.sequelize = sequelize;
   db.sequelize = sequelize;
