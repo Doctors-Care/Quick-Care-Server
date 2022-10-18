893dac451a149a002e4d4e62936ebfdf86b37591
Moez-Temimi <moez.temimi@yahoo.com>
2022-10-18 18:07:18 +0200

    finished setting up socketio

--- Server/Database/models/request.model.js
+++ Server/Database/models/request.model.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       status: {
         type: DataTypes.STRING,
-      
+        
       },
     });
   
--- Server/index.js
+++ Server/index.js
@@ -7,16 +7,32 @@ const express = require("express");
 const db = require("./Database")
 const cors = require ('cors')
 const app = express();
+const http = require ("http")
+const {Server} = require ("socket.io");
 
-var PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 3001
 
-db.sequelize.sync().then(()=>console.log("t3adet")).catch((err)=>console.log(err))
+
+const server = http.createServer(app);
+
+const io = new Server (server,{cors:{origin:"http://192.168.11.223:19000/Chat"}})
+
+io.on("connection", (socket)=>{
+  console.log(`User Connected:${socket.id}`);
+
+  socket.on("send_message", (data)=>{
+console.log(data);
+    socket.broadcast.emit("receive_message", data)
+  })
+})
 app.use(
   cors({
     origin:true,
     credentials: true,
   })
   );
+
+db.sequelize.sync().then(()=>console.log("t3adet")).catch((err)=>console.log(err))
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -27,7 +43,7 @@ app.use("/hce",hceRoutes) ;
 app.use("/ambulance",ambulanceRoute) ;
 app.use("/request",requestRoute)
 
-app.listen(PORT, function () {
+server.listen(PORT, function () {
     console.log(` listening on Port ${PORT} `);
   });
 
